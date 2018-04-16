83bfc217cb7e6ae48f8c56d0cb16662ad4cb3355
ianbarr456 <36930659+ianbarr456@users.noreply.github.com>
2018-04-15 21:37:04 -0700

    Add files via upload

---
+++ Assignment1 - Birthday Card/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CardForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CardForm(Models.BirthdayCardModel birthdayCard)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("BirthdayCardSent", birthdayCard);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Assignment1 - Birthday Card/Models/BirthdayCardModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCardGenerator.Models
+{
+    public class BirthdayCardModel
+    {
+        [Required(ErrorMessage = "Please enter the name of the recipient")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter the name of the sender")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please enter a message")]
+        public string Message { get; set; }
+    }
+}
