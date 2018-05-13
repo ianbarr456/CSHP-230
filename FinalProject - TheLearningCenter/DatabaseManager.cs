5b446dca89f24540fcf4afac8de2420bd180abc3
ianbarr456 <36930659+ianbarr456@users.noreply.github.com>
2018-05-13 00:57:17 -0700

    Add files via upload

---
+++ FinalProject - TheLearningCenter/DatabaseManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TheLearningCenterReworked.Context;
+
+namespace TheLearningCenterReworked
+{
+    public class DatabaseManager
+    {
+        private static readonly TheLearningCenterReworkedEntities entities;
+
+        // Initialize and open the database connection
+        static DatabaseManager()
+        {
+            entities = new TheLearningCenterReworkedEntities();
+            entities.Database.Connection.Open();
+        }
+
+        // Provide an accessor to the database
+        public static TheLearningCenterReworkedEntities Instance
+        {
+            get
+            {
+                return entities;
+            }
+        }
+    }
+}
